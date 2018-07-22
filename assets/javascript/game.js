a1bef357abb02669a1b16806b6771552fc08b5cd
Shmuli Gross <shmuelgross@sams-mbp-2.lan>
2018-07-21 21:12:26 -0500

    I give up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,145 +1,164 @@
-
-
-
-
 // document.getElementById("word-guess").addEventListener("keypress", myFunction);
 
 // function myFunction() {
 //     document.getElementById("word-guess")
 // }
 
-prompt("Press ok to continue")
+alert("Press ok to continue")
+alert("You have 10 chances to prove yourself worthy")
 
 //words used in the game
-var words = [ 'gevald', 'schmaltz', 'schmuck','meshugeh'];
+var words = ['gevald', 'schmaltz', 'schmuck', 'meshugeh'];
 var randomWord = words[Math.floor(Math.random() * words.length)];
-var s;
 var count = 0;
 var answerArray = [];
+var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-function startUp() {
-for (var i = 0; i < randomWord.length; i++) {
-    answerArray[i] = "_";
-}
-   s = answerArray.join(" ");
-   document.getElementById("answer").innerHTML = s;
-}
 
-function Letter(){
-    var letter = document.getElementById("letter").value;
-{
-  if (letter.length > 0){
-      for (var i = 0; i < randomWord.length; i++)
-      {
-          if(randomWord[i] === letter)
-          {
-                answerArray[i] = letter;
-          }
-      }
-    }
-      count++;
-      document.getElementById("counter").innerHTML = "No clicks" + count;
-      document.getElementById("answer").innerHTML = answerArray.join(" ");
-  }
-  if (count>10){
-      document.getElementById("stat").innerHTML = "Oh Bummer"
-  }
-}
-
-// var remainingLetters = randomWord.length;
-
-// document.onkeyup = function(event) {
-//     console.log(event.key)
-//     var letter = event.key
-//     for (var j = 0; j < randomWord.length; j++) {
-//         if (randomWord[j] === guess) {
-//         answerArray[j] = guess;
-//         remainingLetters = remainingLetters-1;
-//         alert('MAATCHING!!!')
-//         console.log(answerArray)
-//         console.log(remainingLetters)
-
-//         } else  {
-           
-//             console.log('wrong guess');
-//         } 
-        
-//        } 
-
+// function startUp() {
+// for (var i = 0; i < randomWord.length; i++) {
+// answerArray.push("_ ");
+// }
+//    s = answerArray.join(" ");
+//    document.getElementById("answer").innerHTML = s;
 // }
 
-
-// while (remainingLetters > 0) {
-    // Game code goes here
-    // Show the player their progress
-    // Take input from the player
-    // Update answerArray and remainingLetters for every correct guess
-// }
-// alert(answerArray.join(" "));
-//  var guess = prompt("Guess a letter, or click Cancel to stop playing.");
-//  if (guess === null) {
-//  break;
-// } else if (guess.length !== 1) {
-//  alert("Please enter a single letter.");
-// } else {
- // Update the game state with the guess
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var name = prompt("What's your name?");
-// console.log("Hello " + name);
-
-// // Create an array of words
-// var words = [
-//         'gevald',
-//         'schmaltz',         
-//         'schmuck',
-//         'meshugeh',
-//     ];
-//     // Pick a random word
-//     var word = words[Math.floor(Math.random() * words.length)];
-//     // Set up the answer array
-//     var answerArray = [];
-//     for (var i = 0; i < word.length; i++) {
-//     answerArray[i] = "_";
+// function Letter(){
+//     alert("dnfdkjnfkjw")
+//     var letter = document.getElementById("letter").value;
+// {
+//   if (letter.length > 0){
+//       for (var i = 0; i < randomWord.length; i++)
+//       {
+//           if(randomWord[i] === letter)
+//           {
+//                 answerArray[i] = letter;
+//           }
+//       }
 //     }
-//     var remainingLetters = word.length;
-//     // The game loop
-//     while (remainingLetters > 0) {
-//     // Show the player their progress
-//     alert(answerArray.join(" "));
-//     // Get a guess from the player
-//     var guess = prompt("Guess a letter, or click Cancel to stop playing");
-//     if (guess === null) {
-//     // Exit the game loop
-//     break;
-//     } else if (guess.length !== 1) {
-//     alert("Please enter a single letter.");
-//     } else {
-//     // Update the game state with the guess
-//     for (var j = 0; j < word.length; j++) {
-//     if (word[j] === guess) {
-//     answerArray[j] = guess;
-//     remainingLetters--;
-//     }
-//     }
-//     }
-//  // The end of the game loop
-// }
-// // Show the answer and congratulate the player
-// alert(answerArray.join(" "));
-// alert("Good job! The answer was " + word)
+//       count++;
+//       document.getElementById("counter").innerHTML = "Number of clicks " + count;
+//       document.getElementById("answer").innerHTML = answerArray.join("_");
+//   }
+
+    //   }
+    // }
+
+    var remainingLetters = randomWord.length;
+
+    document.onkeyup = function (event) {
+            // console.log(event.key)
+            var guess = event.key;
+            if (alphabet.indexOf(guess.toUpperCase()) !== -1) {
+                for (var j = 0; j < randomWord.length; j++) {
+                    if (randomWord[j] === guess ) {
+                        answerArray[j] = guess;
+                        remainingLetters = remainingLetters - 1 ;
+                        alert('MAATCHING!!!')
+                        console.log(answerArray)
+                        console.log(remainingLetters)
+                         
+                        // if they have the whole
+                    } else {
+                        document.getElementById("message").innerHTML= "Guess again. The number is lower than "+guess;
+                    //     guess = alert("Try Again...")
+                    // break;
+                          console.log('wrong guess')
+                             
+                                                               //count wrong guesses     
+                }                         
+                }
+                document.getElementById("answer").innerHTML = answerArray.join(" ");
+                count++;
+                document.getElementById("counter").innerHTML = "Number of clicks " + count;
+            }
+            
+        }
+       
+       
+        // alert("Good job! The answer was " + randomWord);
+
+
+            // if (count > 10) {
+            //     document.getElementById("stat").innerHTML = "Oy Vey!!!"
+
+
+            //if too many wrong guesses
+            //print on page
+
+
+
+            // while (remainingLetters > 0) {
+            // Game code goes here
+            // Show the player their progress
+            // Take input from the player
+            // Update answerArray and remainingLetters for every correct guess
+            // }
+            // alert(answerArray.join(" "));
+            //  var guess = prompt("Guess a letter, or click Cancel to stop playing.");
+            //  if (guess === null) {
+            //  break;
+            // } else if (guess.length !== 1) {
+            //  alert("Please enter a single letter.");
+            // } else {
+            // Update the game state with the guess
+            // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // var name = prompt("What's your name?");
+            // console.log("Hello " + name);
+
+            // // Create an array of words
+            // var words = [
+            //         'gevald',
+            //         'schmaltz',         
+            //         'schmuck',
+            //         'meshugeh',
+            //     ];
+            //     // Pick a random word
+            //     var word = words[Math.floor(Math.random() * words.length)];
+            //     // Set up the answer array
+            //     var answerArray = [];
+            //     for (var i = 0; i < word.length; i++) {
+            //     answerArray[i] = "_";
+            //     }
+            //     var remainingLetters = word.length;
+            //     // The game loop
+            //     while (remainingLetters > 0) {
+            //     // Show the player their progress
+            //     alert(answerArray.join(" "));
+            //     // Get a guess from the player
+            //     var guess = prompt("Guess a letter, or click Cancel to stop playing");
+            //     if (guess === null) {
+            //     // Exit the game loop
+            //     break;
+            //     } else if (guess.length !== 1) {
+            //     alert("Please enter a single letter.");
+            //     } else {
+            //     // Update the game state with the guess
+            //     for (var j = 0; j < word.length; j++) {
+            //     if (word[j] === guess) {
+            //     answerArray[j] = guess;
+            //     remainingLetters--;
+            //     }
+            //     }
+            //     }
+            //  // The end of the game loop
+            // }
+            // // Show the answer and congratulate the player
+            // alert(answerArray.join(" "));
+            // alert("Good job! The answer was " + word)
